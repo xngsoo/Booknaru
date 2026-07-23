ffe91f193cbf362f4e4a519e6a11a61036668167
xngsoo <hanxngsoo@gmail.com>
2026-07-23 16:47:54 +0900

    chore: Tuist 프로젝트 골격 및 모듈 구조 설정

---
+++ Modules/App/Sources/BooknaruApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BooknaruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Booknaru")
+        }
+    }
+}
---
+++ Modules/Domain/Tests/DomainTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import Domain
+
+@Test func domainModuleLoads() {
+    #expect(true)
+}
